e894dcf3191bc0aa7833ae6a716c94b13ba5f688
Spazzlefrazzle <juuncaerlum@gmail.com>
2018-11-08 12:05:03 -0500

    Trying out some basic R commands.

--- hello.R
+++ hello.R
@@ -1,1 +1,13 @@
-cat ( 'Hello, World!\n')
+cat ('Hello, World!\n')
+
+{
+  x <- rnorm(50);
+  y <- rnorm(x);
+  plot(x, y);
+  ls();
+  rm(x,y);
+  x <- 1:20
+  w <- 1 + sqrt(x) / 2;
+  dummy <- data.frame(x=x, y=x+ rnorm(x) * w)
+  dummy
+}
