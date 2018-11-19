49393d4ff5674879ebe4b3e9f0ea0c610d61eaf9
Spazzlefrazzle <juuncaerlum@gmail.com>
2018-11-19 11:14:41 -0500

    Adding a simple scatterplot to the hello app.

--- shiny/code/hello-shiny.R
+++ shiny/code/hello-shiny.R
@@ -3,13 +3,28 @@ library(shiny)
 ui <- fluidPage(
   headerPanel("Hello"),
   sidebarPanel(
-    textInput("id_name", "What is your name?", value="Enamon")
+    textInput("id_name", "What is your name?", value="Enamon"),
+    sliderInput(
+      "id_slider_number", 
+      "# of random points for graph",
+      1,
+      100,
+      50,
+      step = 1
+    )
+    
   ),
-  mainPanel(textOutput("value"))
+  mainPanel(
+    textOutput("value"),
+    plotOutput("scatterplot")
+  )
 )
 
 server <- function(input, output) {
   output$value <- renderText(paste("Hello, ", input$id_name, "!", sep = ""))
+  output$scatterplot <- renderPlot(
+    plot(rnorm(input$id_slider_number, 100, 25), rnorm(input$id_slider_number, 50, 3))
+  )
 }
 
 shinyApp(ui = ui, server = server)
