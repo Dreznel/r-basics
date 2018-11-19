8cdf3519a06510295eb200f7c21c921ae4cf9669
Spazzlefrazzle <juuncaerlum@gmail.com>
2018-11-19 10:53:30 -0500

    Basic Hello World App with input.

--- shiny/code/hello-shiny.R
+++ shiny/code/hello-shiny.R
@@ -2,12 +2,14 @@ library(shiny)
 
 ui <- fluidPage(
   headerPanel("Hello"),
-  sidebarPanel("Inputs"),
-  mainPanel("Outputs?")
+  sidebarPanel(
+    textInput("id_name", "What is your name?", value="Enamon")
+  ),
+  mainPanel(textOutput("value"))
 )
 
 server <- function(input, output) {
-
+  output$value <- renderText(paste("Hello, ", input$id_name, "!", sep = ""))
 }
 
 shinyApp(ui = ui, server = server)
